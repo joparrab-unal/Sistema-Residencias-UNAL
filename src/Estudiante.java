446459aa9117681784a5d9647b03213d16863091
joparrab-unal <joparrab@unal.edu.co>
2026-02-28 16:33:50 -0500

    Update Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,1 +1,10 @@
-
+public class Estudiante {
+  int id;
+  String nombre;
+  int puntajeSocioeconomico;
+ Public Estudiante(int id; Sting nombre, int puntaje) {
+    this.id = id;
+    this.nombre = nombre;
+    this.puntajeSocioeconomico = puntaje;
+  }
+}
