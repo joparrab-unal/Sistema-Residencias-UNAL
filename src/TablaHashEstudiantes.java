752dae0ba11b5f8f35e3bef5ed2addec29338018
joparrab-unal <joparrab@unal.edu.co>
2026-02-28 16:47:31 -0500

    Update TablaHashEstudiantes.java

--- src/TablaHashEstudiantes.java
+++ src/TablaHashEstudiantes.java
@@ -1,1 +1,17 @@
-
+inport java.util.HashMap;
+public class TablasHashEstudiantes {
+  HashMap<Integer, Estudiante> estudiantes;
+  public TablaHashEstudiantes() {
+    estudiantes = new Hashmap<>();
+  }
+  public void insertar(Estudiante e){   // PAra insetar el estudiante 
+    estudiantes.put(e.id, e);
+  }
+  public Estudiante buscar(int id) {   // con esto buscamos al estu
+    retunr estudiantes.get(id);
+  }
+  public void eliminar(int id) {  // y esto par borrarlo 
+    estudiantes.remove(id) ;
+  }
+}
+  
