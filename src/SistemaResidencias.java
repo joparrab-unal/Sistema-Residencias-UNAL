c83f32ce4845f7e229c0f5bc23a79622454e3eb6
jimmysancayetano-droid <jimmysancayetano@gmail.com>
2026-02-28 17:57:20 -0500

    Enhance comments in SistemasResidencias class
    Added comments to clarify the purpose of variables and constructor.

--- src/SistemaResidencias.java
+++ src/SistemaResidencias.java
@@ -1,12 +1,12 @@
 public class SistemasResidencias{
-  TablaHashEstudiantes tabla;
-  MinHeapPrioridad hep;
+  TablaHashEstudiantes tabla; //La tabla hash permite acceder a los estudiantes usando su ID.
+  MinHeapPrioridad hep; //Organiza a los estudiantes degun su puntaje socioeconómico 
   int cuposDisponibles;
 
   public SistemasResidencias(in cupos){
     tabla = new;
   TablaHashEstudiantes();
     heap = new MinHeapPrioridad();
-    cuposDisponibles = cupos;
+    cuposDisponibles = cupos; //Asigna el numero de cupos totales disponobles en el sistema
   }
 }
