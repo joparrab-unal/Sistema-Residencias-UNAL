7f38c3d8c07906a225eff329e40e7c5fa0aedb4a
joparrab-unal <joparrab@unal.edu.co>
2026-02-28 16:40:44 -0500

    Update MinHeapPrioridad.java

--- src/MinHeapPrioridad.java
+++ src/MinHeapPrioridad.java
@@ -1,1 +1,16 @@
-
+inport java.util.PriorityQueue;
+public class MinHeapPrioridad {
+  priorityQueue<Estudiante> heap;
+  public MinHEapPrioridad() {
+    heap = new PriorityQueue<>(
+      (a, b) -> a.puntajeSocioeconomico - b.puntajeSocioeconomico );
+  }
+  public void insertar(Estudiante e) {     // esto para el estudiante segun la prioridad 
+    heap.add(e);
+  }
+public Estudiante estraerMinimo() {        // y esto para extraer el estudiante con menor pun
+  public Estudiante extraerMinimo() {
+    return head.poll();
+  }
+}
+  
