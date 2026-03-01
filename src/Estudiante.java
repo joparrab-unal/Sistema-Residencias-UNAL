22b6b9aa11cdfc6e9414a203173c8063d0fa3ac0
Sergio Quiroga Sandoval <71296519+SergioQS@users.noreply.github.com>
2026-02-28 23:29:20 -0500

    Se arregla el parametro de constructor String (antes "Sting"), se añade un comentario

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,8 +1,9 @@
 public class Estudiante {
+  //Se definen los atributos minimos de la clase Estudiante
   int id;
   String nombre;
   int puntajeSocioeconomico;
- Public Estudiante(int id; Sting nombre, int puntaje) {
+ Public Estudiante(int id; String nombre, int puntaje) {
     this.id = id;
     this.nombre = nombre;
     this.puntajeSocioeconomico = puntaje;
